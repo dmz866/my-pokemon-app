61732f5d034f9b27272e150fe090a457b8d3e35f
David Murillo <dmz866@hotmail.com>
2025-01-08 16:33:01 -0500

    update env file

--- src/ui/pages/login-page/login-page.tsx
+++ src/ui/pages/login-page/login-page.tsx
@@ -11,13 +11,13 @@ type LoginForm = {
 };
 
 export const LoginPage = () => {
+    console.log(process.env)
     const navigate = useNavigate();
     const errorMessage: string = 'Invalid Crendentials';
     const [showErrorMessage, setShowErrorMessage] = useState<boolean>(false);
-    const [loginForm, setLoginForm] = useState<LoginForm>({ email: 'aa', password: 'ffs' });
+    const [loginForm, setLoginForm] = useState<LoginForm>();
     const handleLogin = () => {
-        console.log(loginForm.email)
-        if (!loginForm.email || !loginForm.password) return;
+        if (!loginForm?.email || !loginForm?.password) return;
 
         if (loginForm.email === testUserEmail && loginForm.password === testUserPassword) {
             navigate(HOME_PATH);
@@ -30,7 +30,6 @@ export const LoginPage = () => {
         const fieldName = e.nativeEvent.target.name;
         const fieldValue = e.nativeEvent.target.value;
 
-        console.log({fieldName, fieldValue})
         setLoginForm((prev) => ({ [fieldName]: fieldValue, ...prev, } as LoginForm));
     }
 
@@ -40,11 +39,11 @@ export const LoginPage = () => {
                 <p className='text-3xl font-bold text-white'>Pokemon App</p>
                 <div className="p-4 flex-row text-white">
                     <p>Email</p>
-                    <input name='email' value={loginForm.email} type="text" className="text-black mt-1 px-2 py-1 border rounded-lg" onChange={handleInput} />
+                    <input name='email' type="text" className="text-black mt-1 px-2 py-1 border rounded-lg" onChange={handleInput} />
                 </div>
                 <div className="p-4 flex-row text-white">
                     <p>Password</p>
-                    <input name='password' value={loginForm.password} type="password" className="text-black mt-1 px-2 py-1 border rounded-lg" onChange={handleInput} />
+                    <input name='password' type="password" className="text-black mt-1 px-2 py-1 border rounded-lg" onChange={handleInput} />
                 </div>
                 <div className="p-4">
                     <button className="disabled:bg-gray-200 w-full border rounded-lg bg-green-200" onClick={handleLogin}>Login</button>
